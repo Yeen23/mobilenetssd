d9cee9a77f375fed7c36ff377907476b96c3230a
Yeen23 <89174520+Yeen23@users.noreply.github.com>
2021-11-15 15:08:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,10 +151,10 @@ def event_handle(event):
     if msgType == "text":
         msg = str(event["message"]["text"])
         if msg == "สวัสดี" :
-            replyObj = TextSendMessage(text= "ดีควัฟเบบี๋")
+            replyObj = TextSendMessage(text= "ดีดีจ้า")
         elif msg == "กินข้าวไหม"  :
             replyObj = TextSendMessage(text= "ม่ายยยหละ")
-        elif msg == "แฟนน่ารักจัง" :
+        elif msg == "แมวน่ารักจัง" :
             replyObj = TextSendMessage(text= "จริงจริง")
         elif :
             replyObj = TextSendMessage(text= "ไม่รู้ไม่ชี้") 
